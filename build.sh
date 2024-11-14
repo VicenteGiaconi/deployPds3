d467a01dfdef2eb3174278f24f04748abef00644
VicenteGiaconi <vgiaconi@miuandes.cl>
2024-11-14 07:30:49 -0300

    feat: fa

--- build.sh
+++ build.sh
@@ -5,11 +5,12 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
+python manage.py makemigrations
+
 # Convert static asset files
 python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python manage.py makemigrations
 
 python manage.py migrate
 
